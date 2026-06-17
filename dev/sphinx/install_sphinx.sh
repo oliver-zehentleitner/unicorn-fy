77a894e561d7e915d4186e4fa6ef880dec40b5b8
oliver-zehentleitner <privat@me.com>
2026-06-17 10:00:00 +0200

    docs

--- dev/sphinx/install_sphinx.sh
+++ dev/sphinx/install_sphinx.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+
+python3 -m pip install pip --upgrade
 python3 -m pip install sphinx --upgrade
 python3 -m pip install python-docs-theme-ubs --upgrade
 python3 -m pip install rich --upgrade
